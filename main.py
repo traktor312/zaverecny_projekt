e9a6f1c81c975ab99bc8cc9460a1aed133d84d58
it1701 <36028779+traktor312@users.noreply.github.com>
2020-11-05 13:00:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
-class TestApp(App):
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            ball.velocity_x *= -1.1
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center_x = self.width / 2
+        self.ball.center_y = self.height / 2
+        self.ball.velocity = Vector(4, 0).rotate(randint(-45, 45) + 180 * randint(0, 1))
+
+    def update(self, dt):
+        self.ball.move()
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        if (self.ball.y < 0) or (self.ball.y > self.height - 50):
+            self.ball.velocity_y *= -1
+
+        if self.ball.x < 0:
+            self.serve_ball()
+            self.player2.score += 1
+
+        if self.ball.x > self.width - 50:
+            self.serve_ball()
+            self.player1.score += 1
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 1/4:
+            self.player1.center_y = touch.y
+        if touch.x > self.width * 3/4:
+            self.player2.center_y = touch.y
+
+
+class PongApp(App):
     def build(self):
-        return Button(text='Hello World')
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
-TestApp().run()
+
+PongApp().run()
