356e8fca642930235c36e7a6a45c997027083507
it1701 <36028779+traktor312@users.noreply.github.com>
2020-11-10 17:18:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ from random import randint
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
-    def bounce_ball(self, ball):
+    def bounce_ball(self, ball, width):
         if self.collide_widget(ball):
-            ball.velocity_x *= -1.1
+            direction = -1
+            if self.x < width / 4:
+                direction = 1
+            ball.speed *= 1.1
+            if ball.speed > 35:
+                ball.speed = 35
+            ball.velocity = Vector(direction * ball.speed, 0).rotate(
+                (ball.center_y - self.center_y)/100 * (60 * direction))
 
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+    speed = NumericProperty(4)
+    direction = NumericProperty(1)
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+    def move(self, players, width):
+        s = self.speed
+        while s > 0:
+            self.pos = Vector(self.velocity_x / self.speed, self.velocity_y / self.speed) + self.pos
+            for player in players:
+                player.bounce_ball(self, width)
+            s -= 1
 
 
 class PongGame(Widget):
@@ -29,15 +43,16 @@ class PongGame(Widget):
     player2 = ObjectProperty(None)
 
     def serve_ball(self):
+        self.ball.speed = 4
         self.ball.center_x = self.width / 2
         self.ball.center_y = self.height / 2
-        self.ball.velocity = Vector(4, 0).rotate(randint(-45, 45) + 180 * randint(0, 1))
+        self.ball.velocity = Vector(self.ball.speed, 0).rotate(randint(-45, 45) + 180 * randint(0, 1))
+        #self.ball.velocity = Vector(self.ball.speed, 0)
 
     def update(self, dt):
-        self.ball.move()
+        players = [self.player1, self.player2]
 
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
+        self.ball.move(players, self.width)
 
         if (self.ball.y < 0) or (self.ball.y > self.height - 50):
             self.ball.velocity_y *= -1
